50f244a485e41df24fb29a06d192dd5ba27aaec4
David <david.balbino@slmandic.edu.br>
2020-07-15 14:58:14 -0300

    suport methods, props and api

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
+
     <pre>{{ example }}</pre>
+    <h2>Custom controls</h2>
+
+    <div class="marginBottom">
+      <button @click="$refs.calendar.prevMonth()">Prev Month</button>
+      <button @click="$refs.calendar.nextMonth()">Next Month</button>
+    </div>
+
     <button class="test-button" @click="add">Add test item</button>
-    <Calendar 
+    <Calendar
       :items="items"
       @dayClicked="dayClicked"
       @itemClicked="itemClicked"
@@ -13,6 +21,7 @@
       textButtonPrev="Prev"
       :weeks="weeks"
       :months="months"
+      ref="calendar"
     />
   </div>
 </template>
@@ -85,21 +94,25 @@ export default {
 pre{
   font-size: 20px;
   border: 2px solid #2196f3;
-  width: 450px;
+  max-width: 100%;
   border-left: 12px solid #2196f3;
   border-radius: 5px;
   padding: 14px;
-  
+
   /* Fixed line height */
   line-height: 24px;
-  
+
   /* Use linear-gradient for background image */
   background-image: linear-gradient(180deg, #eee 50%, #fff 50%);
-  
+
   /* Size background so that the height is 2x line-height */
   background-size: 100% 48px;
-  
+
   /* Offset the background along the y-axis by top padding */
   background-position: 0 14px;
 }
+
+  .marginBottom {
+    margin-bottom: 20px;
+  }
 </style>
